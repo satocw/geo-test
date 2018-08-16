19f2d12fa3473b026cfe354396da163834886225
NISHI Satoshi <nishi@flexsche.com>
2018-08-16 12:14:59 +0900

    add plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { Geolocation } from "@ionic-native/geolocation";
+import { BackgroundGeolocation } from "@ionic-native/background-geolocation";
+import { IonicStorageModule } from "@ionic/storage";
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +39,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation,
+    BackgroundGeolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {
+  Geolocation,
+  GeolocationOptions,
+  Geoposition,
+  Coordinates
+} from '@ionic-native/geolocation';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  lastPosition: Coordinates | null = null;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private geolocation: Geolocation
+  ) {}
 
+  getCurrentPosition() {
+    const options: GeolocationOptions = {
+      enableHighAccuracy: true
+    };
+    this.geolocation.getCurrentPosition(options).then((value: Geoposition) => {
+      console.log(value);
+      this.lastPosition = value.coords;
+    });
   }
-
 }
